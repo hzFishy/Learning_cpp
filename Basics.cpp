3c3abce63640370c59661231a56b7d28b0897357
hzFishy <84443782+hzFishy@users.noreply.github.com>
2023-05-26 23:46:14 +0200

    Create Basics.cpp

---
+++ Basics.cpp
@@ -0,0 +1,16 @@
+// Heading (doestn compile if not included)
+#include <iostream>
+
+// Fonction
+int func() {
+  //code
+}
+
+//Print:
+std::cout << "Text \n";
+
+//Compile:
+g++ filename.cpp -o filenametoexecute
+
+//execute:
+./filenametoexecute
