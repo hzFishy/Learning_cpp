5d9312df2b0a50ad7a36143e943f8f09502729b1
hzFishy <84443782+hzFishy@users.noreply.github.com>
2023-05-27 15:45:02 +0200

    Update temperature.cpp

--- Dump/temperature.cpp
+++ Dump/temperature.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
 int main() {
-  double tempf = 66;
-  double tempc = 0;
   
-  tempc = (tempf-32)/1.8;
+  double tempf;
+  double tempc;
+  
+  // Ask the user
+  std::cout << "What is the temp ? \n";
+  std::cin >> tempf;
+  
+  
+  tempc = (tempf - 32) / 1.8;
   
   std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+  
 }
